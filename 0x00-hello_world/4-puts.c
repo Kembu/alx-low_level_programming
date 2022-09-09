2191283415b467f1de56a7bc6e0b3aba3b8af6b1
Kembu <allantoo05@gmail.com>
2022-09-08 19:32:24 -0700

    bug fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-
 /** 
  *  main - printing
  *
  * Return: Always o (successful)
  */
-int main (void)
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
