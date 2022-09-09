1d206932c6389c71f9c8429e564b8fa4cb0e028a
Kembu <allantoo05@gmail.com>
2022-09-08 19:24:16 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/** 
+ *  main - printing
+ *
+ * Return: Always o (successful)
+ */
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return(0);
+
+}
