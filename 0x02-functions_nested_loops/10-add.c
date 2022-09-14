80d647c63553fe710800993ba37db2180edfacfd
Kembu <allantoo05@gmail.com>
2022-09-13 22:50:54 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.c"
+/**
+ * add - adds 2 integers
+ * @num1: number to be used in function
+ * @num2: number to be used in function
+ * Return: result
+ */
+int add(int x , int y)
+{
+	return (x + y);
+}
