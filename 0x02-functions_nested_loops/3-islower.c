f21e21457354c32185e6bff8f10dc0f5a0d93177
Kembu <allantoo05@gmail.com>
2022-09-13 20:40:40 -0700

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main"
+/**
+ * _islower - function to check for lowercase character
+ *
+ * Return: 0
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
