01c04be2aec884a891e40dac743b171efbee43b2
Kembu <allantoo05@gmail.com>
2022-09-13 23:05:19 -0700

    bug fix

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_to_98 - prints all natural numbers to 98
@@ -7,7 +8,7 @@
 
 void print_to_98(int n)
 {
-	if (n n >= 98)
+	if (n >= 98)
 	{
 		while (n > 98)
 		{
