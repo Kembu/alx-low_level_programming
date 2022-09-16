ce937e6be343c859a89f6ed6fa5ca79058934497
Kembu <allantoo05@gmail.com>
2022-09-15 17:25:13 -0700

    task 9 Fizz buzz bug fix

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  *main - program that prints either number
@@ -11,7 +12,11 @@ int main(void)
 
 	for (num = 1; num <= 100; num++)
 	{
-		if ((num % 3 == 0) && (num % 5 == 0))
+		if (num == 100)
+		{
+			printf("Buzz");
+		}
+		else if ((num % 3 == 0) && (num % 5 == 0))
 		{
 			printf("FizzBuzz");
 			printf(" ");
