5a38b9fbae84526e5a0ab50bea2592f9985625f3
Kembu <allantoo05@gmail.com>
2022-09-19 18:21:24 -0700

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,7 +12,9 @@ void puts_half(char *str)
 	int i, len = _strlen(str);
 
 	for (i = ((len - 1) / 2) + 1; i < len; i++)
+	{
 		putchar(*(str + i));
+	}
 		putchar(10);
 }
 
@@ -28,5 +30,6 @@ int _strlen(char *s)
 
 	while (*(s + len) != '\0')
 		len++;
+
 	return (len);
 }
