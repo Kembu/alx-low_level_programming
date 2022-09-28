ab9278d274916113100d18420b636ebf70cc9f34
Kembu <allantoo05@gmail.com>
2022-09-28 16:43:09 -0700

    _strlen_recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns length of a string
+ *
+ * @s: pointer to a string
+ * Return: length of the string
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		result = _strlen_recursion(s + 1) + 1
+			return (result);
+	}
+	else
+		return (0);
+}
