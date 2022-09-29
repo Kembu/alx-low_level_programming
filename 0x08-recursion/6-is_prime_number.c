9d6fbb3b8d8e980f0ea246892822f53e6df914f1
Kembu <allantoo05@gmail.com>
2022-09-28 19:33:18 -0700

    task 6 bug fix

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * _is_prime_number - checks if int is a prime number
+ * is_prime_number - checks if int is a prime number
  *
  * @n: int to be checked
  * Return: 1 if true else 0
  */
 
-int _is_prime_number(int n)
+int is_prime_number(int n)
 {
 	int tmp = 1;
 
