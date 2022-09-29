ab21fe63064d8cc13e137528ffd5bf8eb66a30a8
Kembu <allantoo05@gmail.com>
2022-09-28 17:35:26 -0700

    _pow_recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - prints power of an integer
+ *
+ * @x: integer to be raised
+ * @y: power to be raised to
+ *
+ * Return: result of number raised
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	if (y == 1)
+	{
+		return (x);
+	}
+	else
+		return (x * _pow_recursion(y - 1))
+}
