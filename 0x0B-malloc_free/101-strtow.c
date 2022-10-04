9174443e4a7c052ac620a5d7816253fa3994bb20
Kembu <allantoo05@gmail.com>
2022-10-04 16:50:45 -0700

    bug fix

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -68,17 +68,20 @@ char **strtow(char *str)
 		while (str[index] == ' ')
 			index++;
 			letters = word_len(str + index);
+
 			strings[w] = malloc(sizeof(char) * (letters + 1));
 			if (strings[w] == NULL)
 			{
 				for (; w >= 0; w--)
 					free(strings[w]);
-					free(strings);
+
+				free(strings);
 					return (NULL);
 			}
 			for (l = 0; l < letters; l++)
 				strings[w][l] = str[index++];
-				strings[w][l] = '\0';
+
+			strings[w][l] = '\0';
 	}
 	strings[w] = NULL;
 	return (strings);
