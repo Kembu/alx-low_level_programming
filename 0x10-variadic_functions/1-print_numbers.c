941191b09d94b91a8f2f6a841f03cc3e69e02a1d
Kembu <allantoo05@gmail.com>
2022-10-14 09:27:18 -0700

    function that prints separated by commas

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,32 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - print numbers with commas in between
+ * @separator: string to be printed between numbers
+ * @n: number of integers to be passed
+ *
+ * Return: void.
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list addNumbers;
+	unsigned int i;
+
+	va_start(addNumbers, n);
+
+	if (!separator)
+		return;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(addNumbers, int));
+			if (i != (n - 1))
+			{
+				printf("%s", separator);
+				printf(" ");
+			}
+	}
+	printf("\n");
+}
